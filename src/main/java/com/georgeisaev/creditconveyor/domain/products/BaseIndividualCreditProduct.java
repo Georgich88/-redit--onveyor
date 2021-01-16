923ab137d407c49149437dab3a21b1cb0ed69c1a
igeo <georgeisaev@gmail.com>
2021-01-16 22:36:02 +0700

    annotating the domain as entities

--- src/main/java/com/georgeisaev/creditconveyor/domain/products/BaseIndividualCreditProduct.java
+++ src/main/java/com/georgeisaev/creditconveyor/domain/products/BaseIndividualCreditProduct.java
@@ -1,15 +1,23 @@
 package com.georgeisaev.creditconveyor.domain.products;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import javax.persistence.Id;
+
+import javax.persistence.Entity;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Entity
 public class BaseIndividualCreditProduct implements CreditProduct {
 
+	@Id
 	private final String id;
 	private final String name;
-	private final CreditProductType type;
+	private final BaseCreditProductType type;
 
 	public enum BaseCreditProductType implements CreditProductType {
 		CONSUMER, EXPRESS_LOAN, MICROLOAN, CAR_LOAN, MORTGAGE_LOAN
--- src/main/java/com/georgeisaev/creditconveyor/domain/products/CreditProduct.java
+++ src/main/java/com/georgeisaev/creditconveyor/domain/products/CreditProduct.java
@@ -2,7 +2,7 @@ package com.georgeisaev.creditconveyor.domain.products;
 
 public interface CreditProduct {
 
-	static CreditProduct of(String code, String name, CreditProductType type) {
+	static CreditProduct of(String code, String name, BaseIndividualCreditProduct.BaseCreditProductType type) {
 		return new BaseIndividualCreditProduct(code, name, type);
 	}
 
