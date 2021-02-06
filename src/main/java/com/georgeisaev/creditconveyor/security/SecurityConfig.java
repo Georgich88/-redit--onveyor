c0b33dfdf95bbfe53cb4186ef94d0dd02fac389a
igeo <georgeisaev@gmail.com>
2021-02-06 18:30:56 +0700

    Configuring Spring Security JDBC-based user store

--- src/main/java/com/georgeisaev/creditconveyor/security/SecurityConfig.java
+++ src/main/java/com/georgeisaev/creditconveyor/security/SecurityConfig.java
@@ -1,25 +1,37 @@
 package com.georgeisaev.creditconveyor.security;
 
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.annotations.SqlFragmentAlias;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
+import javax.sql.DataSource;
+
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private final DataSource dataSource;
+
+	@Autowired
+	public SecurityConfig(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		log.warn(bCryptPasswordEncoder.encode("123"));
 		auth
-			.inMemoryAuthentication()
-				.withUser("buzz")
-					.password("{noop}infinity")
-					.authorities("ROLE_USER")
-			.and()
-				.withUser("woody")
-					.password("{noop}bullseye")
-					.authorities("ROLE_USER");
+				.jdbcAuthentication()
+				.dataSource(dataSource)
+				.passwordEncoder(new BCryptPasswordEncoder());
 	}
 
 }
