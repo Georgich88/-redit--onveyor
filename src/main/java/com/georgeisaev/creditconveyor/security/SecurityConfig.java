809feebb7333597bb7e1970812a4fe4894f2a090
igeo <georgeisaev@gmail.com>
2021-02-11 15:59:33 +0700

    Securing requests

--- src/main/java/com/georgeisaev/creditconveyor/security/SecurityConfig.java
+++ src/main/java/com/georgeisaev/creditconveyor/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -34,6 +35,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth
 				.userDetailsService(userDetailsService)
 				.passwordEncoder(encoder());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http.authorizeRequests()
+				.antMatchers("/companies", "/design")
+					.access("hasRole('ROLE_USER')")
+				.antMatchers("/", "/**")
+					.permitAll()
+				.and()
+					.formLogin()
+						.loginPage("/login");
+		// @formatter:on
 	}
 
 }
