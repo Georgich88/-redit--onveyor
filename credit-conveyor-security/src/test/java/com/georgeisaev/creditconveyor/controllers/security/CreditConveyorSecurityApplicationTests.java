63b4fa15bcc5e9479354f4e617f952b1102e0fa1
igeo <georgeisaev@gmail.com>
2021-02-15 17:45:54 +0700

    modularization

---
+++ credit-conveyor-security/src/test/java/com/georgeisaev/creditconveyor/controllers/security/CreditConveyorSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeisaev.creditconveyor.controllers.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditConveyorSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
